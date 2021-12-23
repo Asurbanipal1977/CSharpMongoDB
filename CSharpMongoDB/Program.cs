41b88cc725c5e095dfcb7a7b460771f84efb62a6
Asurbanipal1977 <Asurbanipal1977@gmail.com>
2021-12-23 09:37:18 +0100

    first commit

---
+++ CSharpMongoDB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpMongoDB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
